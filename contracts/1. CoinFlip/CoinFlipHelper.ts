a66819a9333bd8b86c1ffa1689bab2231443b63f
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-10 19:59:03 -0300

    Hacked coinflip

--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -1,8 +1,8 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-
 const helper = async (victim: any, attacker: any) => {
-  // add code here that will help you pass the test
+  for (let i = 0; i < 10; i++) {
+    const tx = await attacker.hackContract();
+    await tx.wait();
+  }
 };
 
 export default helper;
